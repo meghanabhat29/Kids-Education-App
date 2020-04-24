de6a01afe2f614aeb305069102769838ce825449
Vineet Patil <vbpatil9980@gmail.com>
2020-04-24 21:25:40 +0530

    Many to one activity

---
+++ app/src/main/java/com/example/educationapp/ManyOne.java
@@ -0,0 +1,14 @@
+package com.example.educationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManyOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_many_one);
+    }
+}
