bfbc7ecbbe545f2b23afc7055b901efbcf310d93
PoojaK97 <thisispooja97@gmail.com>
2020-04-25 20:30:06 +0530

    connecting

--- app/src/main/java/com/example/educationapp/Dashboard.java
+++ app/src/main/java/com/example/educationapp/Dashboard.java
@@ -1,14 +1,31 @@
 package com.example.educationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Dashboard extends AppCompatActivity {
+
+    CardView speak;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        speak = findViewById(R.id.speak);
+
+        speak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AlphabetDashboard.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
     }
 }
