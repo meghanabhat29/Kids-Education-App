5ce4da48f310e8e1ea683ef2098f540ceddc01f7
PoojaK97 <thisispooja97@gmail.com>
2020-04-30 20:28:22 +0530

    ClockView Set

--- app/src/main/java/com/example/educationapp/TimeReading.java
+++ app/src/main/java/com/example/educationapp/TimeReading.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 
 public class TimeReading extends AppCompatActivity {
 
+    ClockView clock;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_reading);
+
+        clock = findViewById(R.id.clock);
+
     }
 }
