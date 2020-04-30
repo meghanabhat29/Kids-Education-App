2b3019e73764395b9a25f9e509085f57d1f8f0fd
PoojaK97 <thisispooja97@gmail.com>
2020-04-30 19:06:18 +0530

    randomly generating sets

--- app/src/main/java/com/example/educationapp/PutInPlace.java
+++ app/src/main/java/com/example/educationapp/PutInPlace.java
@@ -10,11 +10,16 @@ import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class PutInPlace extends AppCompatActivity {
 
     Button Option1, Option2, Option3, Option4, Option5, Option6, Reset, Submit;
     TextView Question;
-    TextInputEditText Answer[] = new TextInputEditText[5];
+    TextInputEditText Answer[] = new TextInputEditText[6];
     int clicks=0;
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,30 +27,39 @@ public class PutInPlace extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_put_in_place);
 
+        int randomInteger = new Random().nextInt(26) + 65;
+        final char startingAlphabet = (char) randomInteger;
+        Character[] arr = { startingAlphabet, (char) (startingAlphabet+1), (char) (startingAlphabet + 2), (char) (startingAlphabet+3), (char) (startingAlphabet + 4), (char) (startingAlphabet+5)};
+        shuffle(arr);
+
         Option1 = findViewById(R.id.button2);
+        Option1.setText(arr[0].toString());
         Option2 = findViewById(R.id.button4);
+        Option2.setText(arr[1].toString());
         Option3 = findViewById(R.id.button3);
+        Option3.setText(arr[2].toString());
         Option4 = findViewById(R.id.button9);
+        Option4.setText(arr[3].toString());
         Option5 = findViewById(R.id.button7);
+        Option5.setText(arr[4].toString());
         Option6 = findViewById(R.id.button10);
+        Option6.setText(arr[5].toString());
         Submit = findViewById(R.id.button11);
         Reset = findViewById(R.id.buttonReset);
 
-        Question = findViewById(R.id.textView11);
+        Answer[0] = findViewById(R.id.textInputEditText1);
+        Answer[1] = findViewById(R.id.textInputEditText2);
+        Answer[2] = findViewById(R.id.textInputEditText3);
+        Answer[3] = findViewById(R.id.textInputEditText4);
+        Answer[4] = findViewById(R.id.textInputEditText5);
+        Answer[5] = findViewById(R.id.textInputEditText6);
 
-        Answer[0] = findViewById(R.id.textInputEditText2);
-        Answer[1] = findViewById(R.id.textInputEditText3);
-        Answer[2] = findViewById(R.id.textInputEditText4);
-        Answer[3] = findViewById(R.id.textInputEditText5);
-        Answer[4] = findViewById(R.id.textInputEditText6);
-
-        final char startingAlphabet = Question.getText().toString().charAt(0);
 
         Option1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
-                if(clicks!=5)
+                if(clicks!=6)
                 Answer[clicks++].setText(Option1.getText().toString());
                 Option1.setVisibility(View.INVISIBLE);
             }
@@ -55,7 +69,7 @@ public class PutInPlace extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                if(clicks!=5)
+                if(clicks!=6)
                 Answer[clicks++].setText(Option2.getText().toString());
                 Option2.setVisibility(View.INVISIBLE);
             }
@@ -65,7 +79,7 @@ public class PutInPlace extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                if(clicks!=5)
+                if(clicks!=6)
                 Answer[clicks++].setText(Option3.getText().toString());
                 Option3.setVisibility(View.INVISIBLE);
             }
@@ -75,7 +89,7 @@ public class PutInPlace extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                if(clicks!=5)
+                if(clicks!=6)
                 Answer[clicks++].setText(Option4.getText().toString());
                 Option4.setVisibility(View.INVISIBLE);
             }
@@ -85,7 +99,7 @@ public class PutInPlace extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                if(clicks!=5)
+                if(clicks!=6)
                 Answer[clicks++].setText(Option5.getText().toString());
                 Option5.setVisibility(View.INVISIBLE);
             }
@@ -95,7 +109,7 @@ public class PutInPlace extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                if(clicks!=5)
+                if(clicks!=6)
                 Answer[clicks++].setText(Option6.getText().toString());
                 Option6.setVisibility(View.INVISIBLE);
             }
@@ -106,7 +120,7 @@ public class PutInPlace extends AppCompatActivity {
             public void onClick(View v)
             {
                 visible();
-                for(int i=0; i<5; i++)
+                for(int i=0; i<6; i++)
                     Answer[i].setText("");
             }
         });
@@ -118,9 +132,9 @@ public class PutInPlace extends AppCompatActivity {
 
                 try {
 
-                    for (int i = 0; i < 5; i++) {
+                    for (int i = 0; i < 6; i++) {
 
-                        if (Answer[i].getText().toString().charAt(0) == startingAlphabet + i + 1)
+                        if (Answer[i].getText().toString().charAt(0) == startingAlphabet + i)
                             continue;
                         else {
                             Toast.makeText(PutInPlace.this, "WRONG ANSWER", Toast.LENGTH_SHORT).show();
@@ -138,6 +152,12 @@ public class PutInPlace extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void shuffle(Character[] intArray) {
+        List<Character> intList = Arrays.asList(intArray);
+        Collections.shuffle(intList);
+        intList.toArray(intArray);
     }
 
     public void visible()
