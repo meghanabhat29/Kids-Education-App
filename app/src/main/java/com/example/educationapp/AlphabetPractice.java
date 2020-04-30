7d8c6def62312809a05eb105eb2a8b270ba56f8a
CHARIZARD\Developer <neeharchandavarkar4@gmail.com>
2020-04-30 17:03:05 +0530

    REVISE Backend Modified

--- app/src/main/java/com/example/educationapp/AlphabetPractice.java
+++ app/src/main/java/com/example/educationapp/AlphabetPractice.java
@@ -1,20 +1,25 @@
 package com.example.educationapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class AlphabetPractice extends AppCompatActivity {
     TextView mTextTv;
-    ImageButton mSpeakBtn;
+    ImageButton mSpeakBtn, mVoiceBtn;
     TextView mQuestion;
     Button mNext;
     private TextToSpeech mTTS;
@@ -28,6 +33,7 @@ public class AlphabetPractice extends AppCompatActivity {
         setContentView(R.layout.activity_alphabet_practice);
         mTextTv = findViewById(R.id.textTv);
         mQuestion = findViewById(R.id.textViewQuestion);
+        mVoiceBtn = findViewById(R.id.micBtn);
         mNext = findViewById(R.id.buttonNext);
         mSpeakBtn = findViewById(R.id.imageViewSpeak);
 
@@ -60,10 +66,24 @@ public class AlphabetPractice extends AppCompatActivity {
 
 
 
+        mVoiceBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                speak();
+            }
+        });
+
+
+
         mNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
+                mVoiceBtn.setVisibility(View.VISIBLE);
+                mSpeakBtn.setVisibility(View.VISIBLE);
+                mNext.setText("NEXT");
                 if(a==90 || mQuestion.getText().toString().equals("Hello"))
                     a=64;
                 nextQuestion(++a);
@@ -86,6 +106,53 @@ public class AlphabetPractice extends AppCompatActivity {
         mTTS.setPitch((float) 1.0);
         mTTS.setSpeechRate((float) 1.0);
         mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+
+    private void speak()
+    {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi Speak Something");
+
+        try
+        {
+            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+        }
+
+        catch (Exception e)
+        {
+            Toast.makeText(this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode)
+        {
+            case REQUEST_CODE_SPEECH_INPUT:{
+                if(resultCode == RESULT_OK && null!=data)
+                {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String answer = mQuestion.getText().toString().substring(0,1);
+                    if(result.get(0).equalsIgnoreCase(answer) || result.get(1).equalsIgnoreCase(answer)) {
+                        voice("Correct");
+                    }
+                    else {
+                        voice("Incorrect");
+                    }
+                }
+                break;
+
+            }
+
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + requestCode);
+        }
     }
 
     @Override
