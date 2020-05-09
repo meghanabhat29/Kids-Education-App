cde5f1ecb760343b7aefba96ae0804fec8fe56e8
PoojaK97 <thisispooja97@gmail.com>
2020-05-09 17:14:32 +0530

    words

--- app/src/main/java/com/example/educationapp/MainDashboard.java
+++ app/src/main/java/com/example/educationapp/MainDashboard.java
@@ -28,8 +28,8 @@ public class MainDashboard extends AppCompatActivity {
       //  Toast.makeText(MainDashboard.this,"Under Development", Toast.LENGTH_SHORT).show();
     }
     public void Wordfn(View view) {
-        //Intent intent = new Intent(getApplicationContext(), Dashboard.class);
-        //startActivity(intent);
+        Intent intent = new Intent(getApplicationContext(), CategoriesActivity.class);
+        startActivity(intent);
         Toast.makeText(MainDashboard.this,"Under Development", Toast.LENGTH_SHORT).show();
     }
     public void Singularfn(View view) {
---
+++ app/src/main/java/com/example/educationapp/Thing.java
@@ -0,0 +1,50 @@
+package com.example.educationapp;
+
+public class Thing {
+    private int image;
+    private int sound;
+    private String text;
+    private int noise;
+
+    public Thing(int image, int sound, String text, int noise) {
+        this.image = image;
+        this.sound = sound;
+        this.text = text;
+        this.noise = noise;
+    }
+
+    public Thing(int image, int sound, String text) {
+        this(image, sound, text, 0);
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getNoise() {
+        return noise;
+    }
+
+    public boolean hasNoise() {
+        return this.noise != 0;
+    }
+
+    public int getSound() { return sound; }
+
+    public void setSound(int sound) {
+        this.sound = sound;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
+
