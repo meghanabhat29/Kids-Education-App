19bd343ec6ca843eed404421117baa6b849a4ea8
Vineet Patil <vbpatil9980@gmail.com>
2020-05-10 18:04:18 +0530

    Name Activity Update

--- app/src/main/java/com/example/educationapp/RegistrationPage.java
+++ app/src/main/java/com/example/educationapp/RegistrationPage.java
@@ -2,7 +2,9 @@ package com.example.educationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -21,6 +23,14 @@ public class RegistrationPage extends AppCompatActivity {
         email = findViewById(R.id.email);
         password = findViewById(R.id.passwd);
         Register = findViewById(R.id.Signup);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(RegistrationPage.this,Name.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
